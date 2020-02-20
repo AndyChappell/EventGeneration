9c68adb260258f4f4314158fb3ec1c2180088b1c
Isobel Mawby <Isobel.Mawby@warwick.ac.uk>
2020-02-20 15:06:02 +0000

    added Gaussian momentum determination

--- momentum.py
+++ momentum.py
@@ -7,6 +7,7 @@ Created on Mon Jan  6 20:10:51 2020
 """
 
 import numpy as np
+import math
 
 class MomentumSettings :
     
@@ -17,37 +18,52 @@ class MomentumSettings :
         self.stdDev = stdDevList
         self.distHistName = distHistNameList
         
+######################################################################################################
+
+# Fill the particle momentum MAGNITUDE list according to distribution option
 def GetMomentumMag(momentumSettings) :
-    
     if momentumSettings.dist == 0 :
         momentumMagList = GetUniformMomentumMag(momentumSettings)
-        
-    return momentumMagList
+        return momentumMagList
+    if momentumSettings.dist == 1 :
+        momentumMagList = GetGaussianMomentumMag(momentumSettings)
+        return momentumMagList
+
     
-        
+
+# Fill the particle momentum MAGNITUDE list following a uniform distribution 
+# Uniform distribution parameters given in MomentumSettings
 def GetUniformMomentumMag(momentumSettings) :
-    
     momentumMag = np.random.uniform(momentumSettings.mean - momentumSettings.stdDev, momentumSettings.mean + momentumSettings.stdDev)
-    
     return momentumMag
 
+
+# Fill the particle momentum MAGNITUDE list following a gaussian distribution 
+# Gaussian distribution parameters given in MomentumSettings
+def GetGaussianMomentumMag(momentumSettings) :
+    momentumMag = np.random.normal(momentumSettings.mean, momentumSettings.stdDev)
+    return momentumMag
+
+
+######################################################################################################
+
+# Fill momentum VECTOR list such that all momentum vectors lie within a sector
+# Opening angle (angle between sector boundaries) passed 
 def GetMomentumVectorListWithinSector(sectorOpeningAngle, particleMomentumList) :
-    
     momentumVectorList = []
+
     # Get random sector axis for particles to be distributed around
-    
     sectorAxis = GetRandom2DAxis()
     
     for particleMomentum in particleMomentumList :
         momentumXZVector = GetMomentumVectorWithinSector(sectorAxis, sectorOpeningAngle, particleMomentum) 
         momentumVector =  np.insert(momentumXZVector, 1, 0)
-        momentumVectorList.append(momentumVector)
-    
+        momentumVectorList.append(momentumVector)    
     return momentumVectorList
     
 
+# Calculate a momentum VECTOR that lies within the sector
 def GetMomentumVectorWithinSector(sectorAxis, sectorOpeningAngle, particleMomentum) :
-    
     maxAngle = sectorOpeningAngle/2.0
     randomAngle = np.random.uniform(-maxAngle, maxAngle)
     
@@ -58,10 +74,97 @@ def GetMomentumVectorWithinSector(sectorAxis, sectorOpeningAngle, particleMoment
     rotatedAxis = np.dot(rotMatrix, sectorAxis)
     
     momentumVector = particleMomentum * rotatedAxis
-    
     return momentumVector
 
+######################################################################################################
 
+
+# Obtain a random axis in 2D space 
+# (ATTN - CAN POINT IN -VE Z)
 def GetRandom2DAxis() :
     randomAngle = np.random.uniform(-np.pi, np.pi)
     return (np.cos(randomAngle), np.sin(randomAngle))
+
+
+# Fill momentum VECTOR list with random 3D mometum VECTORS 
+# (ATTN - CANNOT POINT IN -VE Z)
+def GetRandom3DMomentumVectorList(particleMomentumMagList) :
+    particleMomentumVecList = []
+    for momentumMag in particleMomentumMagList :
+        particleMomentumVecList.append(GetRandom3DMomentumVector(momentumMag))
+    return particleMomentumVecList
+
+
+# Calculate a random 3D VECTOR given a momentum MAGNITUDE
+# DOES NOT PREVENT POINTING IN NEGATIVE DIRECTIONS
+def GetRandom3DMomentumVector(momentumMag) :
+
+    theta = np.random.uniform(0, np.pi)
+    phi = np.random.uniform(0, 2*np.pi)
+
+    pX = momentumMag*np.cos(phi)*np.sin(theta)
+    pY = momentumMag*np.sin(phi)*np.sin(theta)
+    pZ = momentumMag*np.cos(theta) 
+
+    return [pX, pY, pZ]
+
+
+######################################################################################################
+
+
+def Get3DMomentumVectorListWithAngles(momentumMagList, theta0YZList, delta0YZList, theta0XZList, delta0XZList) :
+    momentumVecList = []
+    for momentumMag, theta0YZ, delta0YZ, theta0XZ, delta0XZ in zip(momentumMagList, theta0YZList, delta0YZList, theta0XZList, delta0XZList) :
+        momentumVec = Get3DMomentumVectorWithAngles(momentumMag, theta0YZ, delta0YZ, theta0XZ, delta0XZ)
+        momentumVecList.append(momentumVec)
+    return momentumVecList
+
+
+def Get3DMomentumVectorWithAngles(momentumMag, theta0YZ, delta0YZ, theta0XZ, delta0XZ) :
+
+    randomTheta0XZ = np.random.uniform(theta0XZ - delta0XZ, theta0XZ + delta0XZ)
+    randomTheta0YZ = np.random.uniform(theta0YZ - delta0YZ, theta0YZ + delta0YZ)
+
+    pX = momentumMag*np.cos(randomTheta0YZ)*np.sin(randomTheta0XZ)
+    pY = momentumMag*np.sin(randomTheta0YZ) 
+    pZ = momentumMag*np.cos(randomTheta0YZ)*np.cos(randomTheta0XZ)
+    return (pX, pY, pZ) 
+
+
+######################################################################################################
+
+
+# Will fill a two particle event
+def Get3DMomentumVectorsWithRandomOpeningAngle(momentumMagList) :
+
+    isForward = False
+    rnd3DAxis = None
+    vecOnCircle = None
+
+    while not isForward :
+
+        openingAngle = np.random.uniform(-np.pi, np.pi)
+
+        theta = np.random.uniform(0, np.pi)
+        phi = np.random.uniform(0, 2*np.pi)
+
+        rnd3DAxis = [np.cos(phi)*np.sin(theta), np.sin(phi)*np.sin(theta), np.cos(theta)]
+
+        rotatedTheta = theta + openingAngle
+
+        vecOnCircle = [np.sin(rotatedTheta)*np.cos(phi), np.sin(rotatedTheta)*np.sin(phi), np.cos(rotatedTheta)]
+  
+        rotationAngle = np.random.uniform(0.0, 2.0*np.pi)
+
+        c = np.cos(rotationAngle)
+        s = np.sin(rotationAngle)
+ 
+        vecRotated = np.dot(vecOnCircle, c) + np.cross(rnd3DAxis, vecOnCircle)*s + np.dot(np.dot(rnd3DAxis, np.dot(rnd3DAxis, vecOnCircle)), 1 - c)
+
+        if (rnd3DAxis[2] + vecRotated[2]) > 0 :
+            isForward = True    
+
+    return [np.dot(rnd3DAxis, momentumMagList[0]), np.dot(vecRotated, momentumMagList[1])]
+
+
+######################################################################################################
--- particleCreationScript.py
+++ particleCreationScript.py
@@ -18,36 +18,52 @@ import numpy as np
 
 # SETTTINGS
 
-# Events to be created
-numberOfEvents = 10
+#Events to be created
+numberOfEvents = 500
 
 # OutputFileName
-eventsFileName = "MuonProtonEvents.txt"
+eventsFileName = "MCParticleData.txt"
 
 # PDG codes of particles to create
-particleList = [14, 2212]
+particleList = [13]
 
 # Masses in GeV
-PDGToMassDict = {14 : 0.106, 2212 : 0.938}
+PDGToMassDict = {13 : 0.106, 2212 : 0.938}
 
 # sectorOpeningAngle defined to be the angle between boundaries of the sector
 # Angle in degrees
-sectorOpeningAngle  = 10
-sectorOpeningAngleRad = np.radians(sectorOpeningAngle)
+#sectorOpeningAngleDeg  = 10
+#sectorOpeningAngle = np.radians(sectorOpeningAngle)
+
+# Opening angle between two particles, given in degrees
+# openingAngleDeg = 40
+# openingAngle = np.radians(openingAngleDeg)
+
+# Theta0YZ angles, input in degrees
+theta0YZListDeg = [0]
+theta0YZList = np.radians(theta0YZListDeg)
+deltaTheta0YZListDeg = [90]
+deltaTheta0YZList = np.radians(deltaTheta0YZListDeg)
+
+# Theta0XZ angles, input in degrees
+theta0XZListDeg = [0]
+theta0XZList = np.radians(theta0XZListDeg)
+deltaTheta0XZListDeg = [90]
+deltaTheta0XZList = np.radians(deltaTheta0XZListDeg)
 
 # Distributions to assign the vertex and particle momentum magnitudes
 # Uniform = 0, Gaussian = 1, Histogram = 2 (momentum only)
 # Momentum lists must be equal in length to the particleList
 vertexDist = 0
-vertexDistMean = [0, 0, 0]       # [x, y, z]
-vertexDistStdDev = [4, 4, 4]     # [x, y, z]
+vertexDistMean = [0, 304, 347]           # [x, y, z]   #DUNE FD [0, 0, 600]         #ProtoDUNE [0, 304, 347]
+vertexDistStdDev = [375, 299, 347]     # [x, y, z]     #DUNE FD [360, 600, 600]     #ProtoDUNE [375, 299, 347]
 
 vertexSettings = vertex.VertexSettings(vertexDist, vertexDistMean, vertexDistStdDev)
-      
-momentumDist = [0, 0]
-momentumDistMean = [0, 0] 
-momentumDistStdDev = [3, 100]
-momentumDistHistName = ["", ""] 
+
+momentumDist = [1]                 
+momentumDistMean = [1]         #DUNEFD 2.5 for Muons, 1 for Protons (Uniform)  #ProtoDUNE 1 (Gaussian)
+momentumDistStdDev = [1]       #DUNEFD 2.5 for Muons, 1 for Protons (Uniform)  #ProtoDUNE 0.05 (Gaussian)
+momentumDistHistName = [""] 
 
 particleMomentumSettings = []
 for dist, mean, stdDev, hist in zip(momentumDist, momentumDistMean, momentumDistStdDev, momentumDistHistName) :
@@ -60,8 +76,8 @@ for dist, mean, stdDev, hist in zip(momentumDist, momentumDistMean, momentumDist
 eventFile = open(eventsFileName, 'w') 
 
 eventNumber = 0
-for event in range() :
-    # Get vertex    
+for event in range(numberOfEvents) :
+    # Get vertex  
     interactionVertex = vertex.GetVertex(vertexSettings)
 
     # Get particle momentum magnitude list    
@@ -75,36 +91,23 @@ for event in range() :
         particleEnergyList.append(math.sqrt(math.pow(p, 2) + math.pow(PDGToMassDict[PDG], 2)))
 
     # Get momentum vectors of particles 
-    particleMomentumVectorList = momentum.GetMomentumVectorListWithinSector(sectorOpeningAngle, particleMomentumMagList)
+    # particleMomentumVectorList = momentum.GetRandom3DMomentumVectorList(particleMomentumMagList)
+    particleMomentumVectorList = momentum.Get3DMomentumVectorListWithAngles(particleMomentumMagList, theta0YZList, deltaTheta0YZList, theta0XZList, deltaTheta0XZList)
+    #particleMomentumVectorList = momentum.Get3DMomentumVectorsWithRandomOpeningAngle(particleMomentumMagList)
 
     # Write event as HEP event format
-    firstLine = str(eventNumber) + str(len(particleList))
+    firstLine = str(eventNumber) + " " + str(len(particleList))
     eventFile.write(firstLine)
+    eventFile.write("\n")
 
-    for PDG, momentumVec, energy in zip(particleList, particleMomentumVectorList, particleEnergyList)
-       secondLine = "1 " + str(PDG) + " 0 0 0 0 " + str(momentumVec[0]) + " " + str(momentumVec[1]) + " " + momentumVec[2] + " " + str(energy) + " " + str(PDGToMassDict[PDG]) + " " + str(interactionVertex[0]) + " " + str(interactionVertex[1]) + " " + str(interactionVertex[2]) + " " + "0" 
+
+    for PDG, momentumVec, energy in zip(particleList, particleMomentumVectorList, particleEnergyList) :
+    
+       secondLine = "1 " + str(PDG) + " 0 0 0 0 " + str(momentumVec[0]) + " " + str(momentumVec[1]) + " " + str(momentumVec[2]) + " " + str(energy) + " " + str(PDGToMassDict[PDG]) + " " + str(interactionVertex[0]) + " " + str(interactionVertex[1]) + " " + str(interactionVertex[2]) + " " + "0.0" 
        eventFile.write(secondLine)
+       eventFile.write("\n")
+
     eventNumber += 1
-
-   
-    print("Particles", particleList)
-    print("Vertex: ", interactionVertex)
-    print("Momentum Magnitudes: ", particleMomentumMagList)
-    print("Energy: ", particleEnergy)
-    print("Momentum Vectors:", particleMomentumVectorList)
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
